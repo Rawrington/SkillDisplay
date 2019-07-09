f719bdf8b999d8cabd1cd16283c056c11246e504
Rawrington <georger1260@gmail.com>
2019-07-09 18:14:20 +0100

    added gh-pages branch for github pages

---
+++ precache-manifest.7b2e40b76553424624942cc4a4077cd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e324c0f70e3687566bd7bd0366532f",
+    "url": "/skilldisplay/index.html"
+  },
+  {
+    "revision": "69970936accdcef39be9",
+    "url": "/skilldisplay/static/css/main.d40aef55.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/skilldisplay/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "69970936accdcef39be9",
+    "url": "/skilldisplay/static/js/main.60476c50.chunk.js"
+  },
+  {
+    "revision": "3872d246eea7a30b8483",
+    "url": "/skilldisplay/static/js/runtime~main.f181b48f.js"
+  }
+]);
