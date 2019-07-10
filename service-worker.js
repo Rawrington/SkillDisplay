61ab5bb5efa5c605c6e6c090bafbd825aa5ca040
Rawrington <georger1260@gmail.com>
2019-07-10 12:15:42 +0100

    update pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SkillDisplay/precache-manifest.c6c751a0d1d976f5df8ef9f5eaab01c0.js"
+  "/SkillDisplay/precache-manifest.2903a9bb3e3230d11a626f341faddc30.js"
 );
 
 self.addEventListener('message', (event) => {
