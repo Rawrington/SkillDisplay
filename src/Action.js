94322dc8045b254bc41a0f9941ead419b3f3a72e
Rawrington <georger1260@gmail.com>
2019-11-03 23:21:37 +0000

    5.1 NIN support fuckery I hate SE grumble grumble

--- src/Action.js
+++ src/Action.js
@@ -17,7 +17,32 @@ const gcdOverrides = new Set([
 	16195, //triple technical finish
 	16196, //quadruple technical finish
 	7418, //flamethrower
-	16483 //tsubame-gaeshi
+	16483, //tsubame-gaeshi
+	2259, //ten
+	18805, 
+	2261, //chi
+	18806,
+	2263, //jin
+	18807,
+	2265, //fuma shurikan
+	18873,
+	18874,
+	18875,
+	2267, //raiton
+	18877,
+	2266, //katon
+	18876,
+	2268, //hyoton
+	18878,
+	16492, //hyosho ranryu
+	16471, //goka meykakku
+	2270, //doton
+	18880,
+	2269, //huton
+	18879,
+	2271, //suiton
+	18881,
+	2272, //rabbit medium
 ])
 
 const ogcdOverrides = new Set([
